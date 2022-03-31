1d5526b91a8890de33810399ca7659238ebb39ce
xinzhifumeng <49949144+xinzhifumeng@users.noreply.github.com>
2022-03-31 23:25:06 +0800

    enter

--- python-test/learn-计算物理/9手写识别.py
+++ python-test/learn-计算物理/9手写识别.py
@@ -8,3 +8,8 @@
     简单的编码数字识别，内容基于 keras 例子：https://keras.io/examples/vision/mnist_convnet/
     任务笔记神经网络（卷积神经网络）'''
 #########################################
+import numpy as np
+from tqdm import tqdm
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as p
